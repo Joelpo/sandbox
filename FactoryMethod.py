d5a04a89369ab5eab0054afcd888c0a5b0474e4c
Joelpo <pestanajoel@gmail.com>
2021-05-06 16:11:29 +0200

    added Factory method design pattern

---
+++ FactoryMethod.py
@@ -0,0 +1,56 @@
+# FactoryMethod.py / Factory method design pattern implementation
+
+from abc import ABC, abstractmethod
+
+# Abstract interface that describes products
+class AbstractProduct(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def create_object():
+            # create obj
+        pass
+
+        # Example product A
+        # a concrete class that implements the Abstract Product interface
+
+
+class ConcreteProductA(AbstractProduct):
+    # a concrete class that implements the Abstract Product interface
+
+    def __init__(self):
+        self.name = "Concrete Product A"
+
+    def create_object(self):
+        return self
+
+# Example product B
+# a concrete class that implements the Abstract Product interface
+class ConcreteProductB(AbstractProduct):
+
+    def __init__(self):
+        self.name = "Concrete Product B"
+
+    def create_object(self):
+        return self
+
+# FACTORY class
+class Creator:
+
+    @staticmethod
+    def create_object(product_type):
+        if product_type == 'a':
+            return ConcreteProductA()
+        if product_type == 'b':
+            return ConcreteProductB()
+        return None
+
+
+# CLIENT
+creator = Creator()
+productA = creator.create_object('a')
+productB = creator.create_object('b')
+productBB = creator.create_object('b')
+print(productA.name)
+print(productB.name)
+print(productBB.name)
